9dead7899fa5b354743a399312f0c9dc02073f14
Ilias Parmaksizoglou <iparmaksizoglou@gmail.com>
2021-06-07 11:29:52 +0300

    changes + litterature

--- benchmark.jl
+++ benchmark.jl
@@ -11,23 +11,33 @@ function solve_model(model,optimizer,time_limit)
 	set_silent(model);
 	optimize!(model);
 	# Print results
+
+	label = split(string(optimizer), ".")
+	if label[1] == "Optimizer"
+		println("Mosek Optimizer Report")
+	else
+		println("$(label[1]) Optimizer Report")
+	end
+
 	@show termination_status(model)
 	@show objective_value(model)
 	@show solve_time(model)
+	println(" ")
 	return
 end
 
 # Reading model from .mps file
-model = read_from_file("random_model.mps");
+model = read_from_file("s250r10.mps.bz2");
+time_limit = 30
 
 # Running CPLEX Solver
-solve_model(model,CPLEX.Optimizer,5)
+solve_model(model,CPLEX.Optimizer,time_limit)
 
 # Running Gurobi Solver
-solve_model(model,Gurobi.Optimizer,5)
+solve_model(model,Gurobi.Optimizer,time_limit)
 
 # Running Xpress Solver
-solve_model(model,Xpress.Optimizer,5)
+solve_model(model,Xpress.Optimizer,time_limit)
 
 # Running Mosek Solver
-solve_model(model,Mosek.Optimizer,5)
+solve_model(model,Mosek.Optimizer,time_limit)
